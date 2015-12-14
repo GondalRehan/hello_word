54b44ac0f334e967440c53aa3823117672769d54
GondalRehan <rehanahmad123@yahoo.com>
2015-12-14 22:00:44 +0000

    First hello world

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;}
+{\colortbl;\red255\green255\blue255;\red230\green230\blue230;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs26 \cf0 \cb2 \expnd0\expndtw0\kerning0
+public class HelloWorld \{ \
+   public static void main(String[] args) \{ \
+      System.out.println("Hello, World");\
+   \}\
+\}}
